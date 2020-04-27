536d8af46f950500615e3369430ba4079bed2c28
Jasdeep Singh <jasdeep.singh@comprotechnologies.com>
2020-04-27 14:47:20 +0530

    Updating LO

--- assets/public/LO-1/data/consumerConfig.js
+++ assets/public/LO-1/data/consumerConfig.js
@@ -1,25 +1,21 @@
 var consumerConfig = {
-  "players": {
-    "playerMap": {
-      "flashcard": "flashcard-activity-player",
-      "presentation-card": "presentation-card-item-player"
-    },
+  "renderers": {
     "modules": {
       "flashcard-activity-player": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/flashcard-activity-player/releases/",
-        "version": "0.0.6",
-        "js": "0.0.6/libs-flashcard-activity-player",
-        "css": "0.0.6/libs-flashcard-activity-player",
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/flashcard-activity-player/releases/",
+        "version": "0.0.8",
+        "js": "0.0.8/libs-flashcard-activity-player",
+        "css": "0.0.8/libs-flashcard-activity-player",
         "dependencies": [
           "react",
           "react-dom"
         ]
       },
       "presentation-card-item-player": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/presentation-card-item-player/releases/",
-        "version": "0.0.4",
-        "js": "0.0.4/libs-presentation-card-item-player",
-        "css": "0.0.4/libs-presentation-card-item-player",
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/presentation-card-item-player/releases/",
+        "version": "0.0.5",
+        "js": "0.0.5/libs-presentation-card-item-player",
+        "css": "0.0.5/libs-presentation-card-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -27,19 +23,19 @@ var consumerConfig = {
         ]
       },
       "react": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/react/releases/",
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/react/releases/",
         "version": "16.12.0",
         "js": "16.12.0/react.development"
       },
       "react-dom": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/react-dom/releases/",
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/react-dom/releases/",
         "version": "16.12.0",
         "js": "16.12.0/react-dom.development"
       },
       "libs-player-ui-components": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/libs-player-ui-components/releases/",
-        "version": "0.0.15",
-        "js": "0.0.15/libs-player-ui-components"
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/libs-player-ui-components/releases/",
+        "version": "0.0.16",
+        "js": "0.0.16/libs-player-ui-components"
       }
     }
   }
