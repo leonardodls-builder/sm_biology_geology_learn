7de4ac0b625fd69ac83dfbaeb2ed764aedbafe5b
Babanpreet Singh <babanpreet.singh@comprotechnologies.com>
2020-06-12 14:18:49 +0530

    Synching Consumerconfig as per develop branch

--- assets/public/LO-1/data/consumerConfig.js
+++ assets/public/LO-1/data/consumerConfig.js
@@ -3,19 +3,20 @@ var consumerConfig = {
     "modules": {
       "flashcard-activity-player": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/flashcard-activity-player/releases/",
-        "version": "0.0.10",
-        "js": "0.0.10/libs-flashcard-activity-player",
-        "css": "0.0.10/libs-flashcard-activity-player",
+        "version": "0.0.13",
+        "js": "0.0.13/libs-flashcard-activity-player",
+        "css": "0.0.13/libs-flashcard-activity-player",
         "dependencies": [
           "react",
-          "react-dom"
+          "react-dom",
+          "libs-player-ui-components"
         ]
       },
       "presentation-card-item-player": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/presentation-card-item-player/releases/",
-        "version": "0.0.8",
-        "js": "0.0.8/libs-presentation-card-item-player",
-        "css": "0.0.8/libs-presentation-card-item-player",
+        "version": "0.0.10",
+        "js": "0.0.10/libs-presentation-card-item-player",
+        "css": "0.0.10/libs-presentation-card-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -34,8 +35,8 @@ var consumerConfig = {
       },
       "libs-player-ui-components": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/libs-player-ui-components/releases/",
-        "version": "0.0.16",
-        "js": "0.0.16/libs-player-ui-components"
+        "version": "0.3.1",
+        "js": "0.3.1/libs-player-ui-components"
       }
     }
   }
